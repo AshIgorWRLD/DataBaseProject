d975abf6ecff4dad204e9057b940368750a26a49
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2022-05-23 00:00:14 +0700

    all tables crud ready

---
+++ music_studio_app/src/main/java/ru/nsu/ashikhmin/music_studio_app/MusicStudioAppApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.ashikhmin.music_studio_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStudioAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStudioAppApplication.class, args);
+    }
+
+}
---
+++ music_studio_app/src/test/java/ru/nsu/ashikhmin/music_studio_app/MusicStudioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.ashikhmin.music_studio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStudioAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
